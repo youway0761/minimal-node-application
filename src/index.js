dce2921bbb9d2a4357bd8bb3160fa56e22cab768
youway0761 <youway@hotmail.com>
2022-04-20 16:04:43 +0800

    For graphql API and ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,13 +1,117 @@
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+          }`,
+      },
+    });
+  },
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+/* const GET_ORGANIZATION = gql`
+{
+  organization(login: "the-road-to-learn-react") {
+    name
+    url
+  }
+}
+`; */
 
-console.log(user);
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!, $cursor: String) {
+    organization(login: $organization) {
+      name
+      url
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
+      ) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+  fragment repository on Repository {
+    name
+    url
+  }
+`;
 
-console.log(process.env.SOME_ENV_VARIABLE);
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: { starrableId: $repositoryId}) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: {
+      organization: 'the-road-to-learn-react',
+      cursor: undefined,
+    },
+  })
+  // resolve first page
+  .then(result => {
+    const { pageInfo, edges } = result.data.organization.repositories;
+    const { endCursor, hasNextPage } = pageInfo;
+
+    console.log('second page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  })
+  // query second page
+  .then(({ endCursor, hasNextPage }) => {
+    if (!hasNextPage) {
+      throw Error('no next page');
+    }
+
+    return client.query({
+      query: GET_REPOSITORIES_OF_ORGANIZATION,
+      variables: {
+        organization: 'the-road-to-learn-react',
+        cursor: endCursor,
+      },
+    });
+  })
+  // resolve second page
+  .then(result => {
+    const { pageInfo, edges } = result.data.organization.repositories;
+    const { endCursor, hasNextPage } = pageInfo;
+
+    console.log('second page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  })
+  // log error when there is no next page
+  .catch(console.log);
+
+client
+  .mutate({
+    mutation: ADD_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    },
+  })
+  .then(console.log);
